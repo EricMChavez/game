c9abb2df0c9e1383cf78ffdaaacc1a206efd9eb0
EricMChavez <eric@emcaudio.com>
2019-04-14 18:22:45 -0500

    added obs

--- index.js
+++ index.js
@@ -19,6 +19,37 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+//
+//
+//enemies
+class Enemies extends Phaser.GameObjects.Sprite {
+	constructor(scene, x, y) {
+		super(scene, x, y);
+		this.scene = scene;
+		this.scene.add.existing(this);
+		this.scene.physics.world.enableBody(this, 0);
+		this.body.setCollideWorldBounds(true);
+		this.patrol = function() {
+			if (this.body.blocked.right) {
+				this.body.velocity.x = -this.speed;
+				this.flipX = false;
+			} else if (this.body.blocked.left) {
+				this.flipX = true;
+				this.body.velocity.x = this.speed;
+			}
+		};
+	}
+}
+class Slime extends Enemies {
+	constructor(scene, x, y) {
+		super(scene, x, y);
+		this.anims.play('walk', true);
+		this.setScale(2);
+		this.speed = 50;
+		this.body.setVelocityX(-this.speed);
+	}
+}
+
 function preload() {
 	this.load.image('background-0', 'assets/background/layer-0.png');
 	this.load.image('background-1', 'assets/background/layer-1.png');
@@ -37,6 +68,8 @@ function preload() {
 	this.load.tilemapTiledJSON('map', 'assets/tilemap.json');
 	this.load.image('tile', 'assets/tile.png');
 	this.load.image('spark', 'assets/blueParticle.png');
+	this.load.image('platform', 'assets/platform.png');
+	this.load.spritesheet('door', 'assets/door.png', { frameWidth: 18, frameHeight: 48 });
 }
 
 function create() {
@@ -54,23 +87,6 @@ function create() {
 
 	worldLayer = map.createDynamicLayer('tileLayer', 'tile', 0, 0).setScale(1);
 	worldLayer.setCollisionByExclusion([ -1 ]);
-
-	hearts = this.add.sprite(20, 20, 'hearts').setScrollFactor(0).setOrigin(0, 0).setDisplaySize(170, 34);
-
-	player = this.physics.add.sprite(7000, 650, 'dude').setDisplaySize(124, 92);
-	player.justHurt = false;
-	player.justFired = false;
-
-	enemies = this.physics.add.sprite(1600, 650, 'slime').setDisplaySize(65, 50);
-	var particles = this.add.particles('spark');
-
-	emitter = particles.createEmitter({ scale: { start: 0.1, end: 0 } });
-
-	emitter.setSpeed(90);
-	emitter.setBlendMode(Phaser.BlendModes.ADD);
-	egg = this.physics.add.sprite(7250, 120, 'egg').setOrigin(0, 0).setDisplaySize(60, 77);
-	emitter.setPosition(egg.x + 30, egg.y + 50);
-
 	this.anims.create({
 		key: 'left',
 		frames: this.anims.generateFrameNumbers('dude', { start: 8, end: 13 }),
@@ -174,23 +190,128 @@ function create() {
 		key: 'heart-10',
 		frames: this.anims.generateFrameNumbers('hearts', { start: 9, end: 9 })
 	});
-	enemies.setCollideWorldBounds(true);
-	enemies.setBounce(0.6);
-	enemies.setSize(20, 15).setOffset(8, 10);
+	hearts = this.add.sprite(20, 20, 'hearts').setScrollFactor(0).setOrigin(0, 0).setDisplaySize(170, 34);
+
+	player = this.physics.add.sprite(6000, 150, 'dude').setDisplaySize(124, 92);
+	player.justHurt = false;
+	player.justFired = false;
+
+	enemies = this.add.group();
+
+	slimes = new Slime(this, 2000, 300);
+	slime2 = new Slime(this, 3000, 300);
+	enemies.add(slimes);
+	enemies.add(slime2);
+	platform1 = this.physics.add.image(4380, 500, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
+	platform1.body.setAllowGravity(false);
+	platform2 = this.physics.add.image(4380, 550, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
+	platform2.body.setAllowGravity(false);
+	platform3 = this.physics.add.image(4880, 500, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
+	platform3.body.setAllowGravity(false);
+	platform4 = this.physics.add.image(4880, 550, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
+	platform4.body.setAllowGravity(false);
+	platform5 = this.physics.add.image(5800, 400, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
+	platform5.body.setAllowGravity(false);
+
+	this.tweens.timeline({
+		targets: [ platform1.body.velocity, platform3.body.velocity ],
+		loop: -1,
+		tweens: [
+			{
+				x: -200,
+				y: 50,
+				duration: 1000,
+				ease: 'Stepped'
+			},
+			{
+				x: 200,
+				y: 50,
+				duration: 1000,
+				ease: 'Stepped'
+			},
+			{
+				x: 200,
+				y: -50,
+				duration: 1000,
+				ease: 'Stepped'
+			},
+			{
+				x: -200,
+				y: -50,
+				duration: 1000,
+				ease: 'Stepped'
+			}
+		]
+	});
+	this.tweens.timeline({
+		targets: [ platform2.body.velocity, platform4.body.velocity ],
+		loop: -1,
+		tweens: [
+			{
+				x: 200,
+				y: -50,
+				duration: 1000,
+				ease: 'Stepped'
+			},
+			{
+				x: -200,
+				y: -50,
+				duration: 1000,
+				ease: 'Stepped'
+			},
+			{
+				x: -200,
+				y: 50,
+				duration: 1000,
+				ease: 'Stepped'
+			},
+			{
+				x: 200,
+				y: 50,
+				duration: 1000,
+				ease: 'Stepped'
+			}
+		]
+	});
+	this.tweens.timeline({
+		targets: [ platform5.body.velocity ],
+		loop: -1,
+		tweens: [
+			{
+				x: 0,
+				y: 0,
+				duration: 700,
+				ease: 'Stepped'
+			},
+			{
+				x: -150,
+				y: 0,
+				duration: 3000,
+				ease: 'Stepped'
+			},
+			{
+				x: 0,
+				y: 0,
+				duration: 700,
+				ease: 'Stepped'
+			},
+			{
+				x: 150,
+				y: 0,
+				duration: 3000,
+				ease: 'Stepped'
+			}
+		]
+	});
+	doors = this.physics.add.sprite(6280, 315, 'door').setImmovable(true).setScale(2.3);
+	doors.body.setAllowGravity(false);
+	doors.open = function() {};
 
 	player.setCollideWorldBounds(true);
 	player.setSize(20, 30).setOffset(15, 5);
-	this.physics.add.collider(worldLayer, player);
+	this.physics.add.collider(worldLayer, [ player, enemies ]);
+	this.physics.add.collider([ platform1, platform2, platform3, platform4, platform5, doors ], player);
 
-	function clash() {
-		enemies.anims.play('attack', true);
-		if (action == 'slash') {
-			enemies.disableBody();
-			enemies.anims.play('die', true);
-		} else if (enemies.anims.currentFrame.index == 5 && enemies.anims.currentAnim.key == 'attack') {
-			player.hurt();
-		}
-	}
 	function hit() {
 		/* arrow hit action */
 	}
@@ -234,8 +355,6 @@ function create() {
 		runChildUpdate: true
 	});
 
-	this.physics.add.collider(player, enemies, clash);
-	this.physics.add.collider(arrows, enemies, hit);
 	worldLayer.setTileIndexCallback(13, hit, this);
 
 	player.health = 10;
@@ -257,6 +376,12 @@ function create() {
 			console.log('endgame');
 		}
 	};
+	var particles = this.add.particles('spark');
+	egg = this.physics.add.sprite(7250, 120, 'egg').setOrigin(0, 0).setDisplaySize(60, 77);
+	emitter = particles.createEmitter({ scale: { start: 0.1, end: 0 } });
+	emitter.setSpeed(90);
+	emitter.setBlendMode(Phaser.BlendModes.ADD);
+	emitter.setPosition(egg.x + 30, egg.y + 50);
 
 	z = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
 	x = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
@@ -267,7 +392,7 @@ function create() {
 	this.cameras.main.startFollow(player, true, 0.01, 0.01);
 }
 action = 'idol';
-//
+var previousPosition;
 //
 //
 function update() {
@@ -279,18 +404,11 @@ function update() {
 		egg.setGravity(0, -1950);
 	}
 	emitter.setPosition(egg.x + 30, egg.y + 50);
-	// enemies
-	if (player.x > enemies.x) {
-		enemies.setVelocityX(50);
-		enemies.flipX = true;
-	} else {
-		enemies.setVelocityX(-50);
-		enemies.flipX = false;
-	}
-	if ((enemies.x > player.x + 50 || enemies.x < player.x - 50) && enemies.body.enable == true) {
-		enemies.anims.play('walk', true);
-	}
 	updateHealth(player.health);
+
+	for (let i in enemies.getChildren()) {
+		enemies.getChildren()[i].patrol();
+	}
 	//background movement
 	this.background2._tilePosition.x = -(this.cameras.main.midPoint.x * 0.1);
 	this.background3._tilePosition.x = -(this.cameras.main.midPoint.x * 0.2);
@@ -299,14 +417,13 @@ function update() {
 	this.background6._tilePosition.x = -(this.cameras.main.midPoint.x * 0.5);
 	this.background7._tilePosition.x = -this.cameras.main.midPoint.x;
 }
-var previousPosition;
 
 //
 function updateHealth(health) {
 	hearts.anims.play(`heart-${health}`, true);
 }
 function playerControls() {
-	if (previousPosition > 0 && player.body.onFloor()) {
+	if (previousPosition > 0 && (player.body.onFloor() || player.body.touching.down)) {
 		action = 'idol';
 	}
 	if (cursors.left.isDown) {
@@ -317,7 +434,7 @@ function playerControls() {
 	}
 	if (cursors.down.isDown) {
 		player.anims.play('duck', true);
-		if (player.body.onFloor()) {
+		if (player.body.onFloor() || player.body.touching.down) {
 			player.setVelocityX(0);
 		}
 	} else if (cursors.left.isDown) {
@@ -341,14 +458,14 @@ function playerControls() {
 	//Jump
 	if (cursors.up.isDown && action == 'idol') {
 		player.anims.play('jump', true);
-		if (player.body.onFloor()) {
+		if (player.body.onFloor() || player.body.touching.down) {
 			action = 'jump';
 			player.setVelocityY(-900);
 		}
 	}
 	//slash
 	if (Phaser.Input.Keyboard.JustDown(x) && (action == 'idol' || action == 'jump')) {
-		if (player.body.onFloor()) {
+		if (player.body.onFloor() || player.body.touching.down) {
 			player.setVelocityX(0);
 		}
 		player.anims.play('slash', true);
@@ -362,14 +479,25 @@ function playerControls() {
 		player.justFired = true;
 		var arrow = arrows.get();
 		arrow.fire(player.x, player.y, player.flipX);
+		doors.setFrame(1);
+		doors.body.setEnable(false);
+		console.dir(doors);
 		setTimeout(function() {
 			player.justFired = false;
 		}, 500);
 	}
-	if (Phaser.Input.Keyboard.JustUp(z) && player.body.onFloor() && (action == 'shoot' || action == 'idol')) {
+	if (
+		Phaser.Input.Keyboard.JustUp(z) &&
+		(player.body.onFloor() || player.body.touching.down) &&
+		(action == 'shoot' || action == 'idol')
+	) {
 		action = 'idol';
 	}
-	if (Phaser.Input.Keyboard.JustDown(z) && player.body.onFloor() && (action == 'shoot' || action == 'idol')) {
+	if (
+		Phaser.Input.Keyboard.JustDown(z) &&
+		(player.body.onFloor() || player.body.touching.down) &&
+		(action == 'shoot' || action == 'idol')
+	) {
 		action = 'shoot';
 		player.setVelocityX(0);
 		player.anims.play('shoot', true);
