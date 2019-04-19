a4d4881024a74a6aac24b2bf1a8a9004bd5baecf
EricMChavez <eric@emcaudio.com>
2019-04-18 21:39:22 -0500

    added minotaur

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var config = {
 	physics: {
 		default: 'arcade',
 		arcade: {
+			tileBias: 64,
 			gravity: { y: 2000 },
 			debug: false
 		}
@@ -26,16 +27,42 @@ class Enemies extends Phaser.GameObjects.Sprite {
 	constructor(scene, x, y) {
 		super(scene, x, y);
 		this.scene = scene;
+		this.flipX = true;
 		this.scene.add.existing(this);
 		this.scene.physics.world.enableBody(this, 0);
 		this.body.setCollideWorldBounds(true);
+		this.alerted = false;
+		this.justAttacked = false;
 		this.patrol = function() {
-			if (this.body.blocked.right) {
-				this.body.velocity.x = -this.speed;
-				this.flipX = false;
-			} else if (this.body.blocked.left) {
-				this.flipX = true;
-				this.body.velocity.x = this.speed;
+			if (
+				!(player.y > this.body.y + 100 || player.y < this.body.y - 100) &&
+				!(player.x > this.body.x + 300 || player.x < this.body.x - 300)
+			) {
+				this.alerted = true;
+			}
+			if (player.x > this.body.x + 100 || player.x < this.body.x - 50) {
+				this.walk();
+			}
+			if (this.alerted == false) {
+				if (this.body.blocked.right) {
+					this.body.velocity.x = -this.speed;
+					this.flipX = true;
+				} else if (this.body.blocked.left) {
+					this.flipX = false;
+					this.body.velocity.x = this.speed;
+				}
+			}
+			this.alert();
+		};
+		this.alert = function() {
+			if (this.alerted == true && this.justAttacked == false) {
+				if (player.x > this.body.x) {
+					this.flipX = false;
+					this.body.velocity.x = this.speed;
+				} else {
+					this.body.velocity.x = -this.speed;
+					this.flipX = true;
+				}
 			}
 		};
 	}
@@ -43,10 +70,122 @@ class Enemies extends Phaser.GameObjects.Sprite {
 class Slime extends Enemies {
 	constructor(scene, x, y) {
 		super(scene, x, y);
-		this.anims.play('walk', true);
+		this.walk = function() {
+			if (this.alive == true) {
+				this.anims.play('slime-walk', true);
+			}
+		};
 		this.setScale(2);
 		this.speed = 50;
+		this.alive = true;
 		this.body.setVelocityX(-this.speed);
+		this.body.setMass(10);
+		this.body.setSize(20, 15);
+		this.body.setOffset(5, 9);
+		this.attack = function() {
+			if (this.alive == true) {
+				this.anims.play('slime-attack', true);
+				if (this.anims.currentFrame.index == 5 && this.anims.currentAnim.key == 'slime-attack') {
+					player.hurt();
+				}
+			}
+		};
+		this.hurt = function() {
+			this.alive = false;
+
+			this.anims.play('slime-die', true);
+			setTimeout(() => {
+				this.destroy();
+			}, 550);
+		};
+	}
+}
+class Minotaur extends Enemies {
+	constructor(scene, x, y) {
+		super(scene, x, y);
+		this.speed = 75;
+		this.alive = true;
+		this.setScale(2);
+		this.body.setMass(10);
+		this.body.setSize(35, 40);
+		this.body.setOffset(32, 22);
+		this.justHurt = false;
+		this.health = 4;
+		this.walk = function() {
+			if (this.alive == true) {
+				if (this.alerted == true) {
+					this.anims.play('minotaur-walk', true);
+				} else {
+					this.anims.play('minotaur-idol', true);
+				}
+			}
+		};
+		this.hurt = function() {
+			if (this.health > 0 && this.justHurt == false) {
+				this.setTintFill(0xffffff);
+				setTimeout(() => {
+					this.clearTint();
+				}, 100);
+				this.justHurt = true;
+				setTimeout(() => {
+					this.justHurt = false;
+				}, 1000);
+				return --this.health;
+			} else if (this.health <= 0) {
+				this.alive = false;
+				this.anims.play('minotaur-die', true);
+				setTimeout(() => {
+					this.destroy();
+				}, 1000);
+				this.justAttacked = true;
+			}
+		};
+		this.attack = function() {
+			if (this.alive == true && this.justAttacked == false) {
+				this.justAttacked = true;
+				this.body.velocity.x = 0;
+				this.anims.play('minotaur-attack', true);
+				if (this.anims.currentFrame.index == 4 && this.anims.currentAnim.key == 'minotaur-attack') {
+					player.hurt();
+				}
+			}
+			setTimeout(() => {
+				this.justAttacked = false;
+			}, 800);
+		};
+		this.alert = function() {
+			if (this.alerted == true && this.justAttacked == false) {
+				if (player.x > this.body.x) {
+					this.flipX = false;
+					this.body.velocity.x = this.speed;
+				} else {
+					this.body.velocity.x = -this.speed;
+					this.flipX = true;
+				}
+			} else {
+				this.body.velocity.x = 0;
+			}
+		};
+	}
+}
+class Arrow extends Phaser.GameObjects.Sprite {
+	constructor(scene) {
+		super(scene);
+		this.setTexture('arrow');
+		this.scene.add.existing(this);
+		this.scene.physics.world.enableBody(this, 0).setScale(1.5);
+		this.body.setAllowGravity(false);
+		this.fire = function(x, y, flip) {
+			if (flip == true) {
+				this.setPosition(x - 30, y + 5);
+				this.flipX = true;
+				this.body.setVelocityX(-1000);
+			} else {
+				this.setPosition(x + 30, y + 5);
+				this.flipX = false;
+				this.body.setVelocityX(1000);
+			}
+		};
 	}
 }
 
@@ -59,10 +198,14 @@ function preload() {
 	this.load.image('background-5', 'assets/background/layer-5.png');
 	this.load.image('background-6', 'assets/background/layer-6.png');
 	this.load.image('background-7', 'assets/background/layer-7.png');
+	this.load.image('arrowkeys', 'assets/keyboardArrow.png');
+	this.load.image('keyX', 'assets/keyboardX.png');
+	this.load.image('keyZ', 'assets/keyboardZ.png');
 	this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 50, frameHeight: 37 });
 	this.load.spritesheet('dude-bow', 'assets/dude-bow.png', { frameWidth: 50, frameHeight: 37 });
 	this.load.spritesheet('hearts', 'assets/hearts-5.png', { frameWidth: 85, frameHeight: 17 });
 	this.load.spritesheet('slime', 'assets/slime.png', { frameWidth: 32, frameHeight: 25 });
+	this.load.spritesheet('minotaur', 'assets/minotaur.png', { frameWidth: 96, frameHeight: 96 });
 	this.load.image('arrow', 'assets/arrow.png');
 	this.load.image('egg', 'assets/egg.png');
 	this.load.tilemapTiledJSON('map', 'assets/tilemap.json');
@@ -70,10 +213,12 @@ function preload() {
 	this.load.image('spark', 'assets/blueParticle.png');
 	this.load.image('platform', 'assets/platform.png');
 	this.load.spritesheet('door', 'assets/door.png', { frameWidth: 18, frameHeight: 48 });
+	this.load.spritesheet('switch', 'assets/switch.png', { frameWidth: 16, frameHeight: 16 });
+	this.load.image('bow', 'assets/bow.png');
 }
 
 function create() {
-	this.physics.world.setBounds(0, 0, 8000, 740);
+	this.physics.world.setBounds(0, 0, 8000, 735);
 	this.background7 = this.add.tileSprite(0, 0, 8000, config.height, 'background-7').setOrigin(0, 0);
 	this.background6 = this.add.tileSprite(0, 0, 8000, config.height, 'background-6').setOrigin(0, 0);
 	this.background5 = this.add.tileSprite(0, 0, 8000, config.height, 'background-5').setOrigin(0, 0);
@@ -81,12 +226,18 @@ function create() {
 	this.background3 = this.add.tileSprite(0, 0, 8000, config.height, 'background-3').setOrigin(0, 0);
 	this.background2 = this.add.tileSprite(0, 0, 8000, config.height, 'background-2').setOrigin(0, 0);
 	this.background1 = this.add.tileSprite(0, 0, 8000, config.height, 'background-1').setOrigin(0, 0);
-
+	this.add.image(450, 310, 'keyX').setScale(3);
+	this.add.image(900, 300, 'arrowkeys').setScale(3);
+	keyHold = this.add.text(3700, 295, 'HOLD', { fontFamily: 'Arial', fontSize: 24, color: '#d6bb9a' });
+	keyY = this.add.image(3830, 310, 'keyZ').setScale(3);
+	bow = this.physics.add.staticImage(3788, 250, 'bow').setScale(2);
 	const map = this.make.tilemap({ key: 'map' });
 	const tiles = map.addTilesetImage('tile', 'tile', 16, 16);
-
+	keyHold.visible = false;
+	keyY.visible = false;
 	worldLayer = map.createDynamicLayer('tileLayer', 'tile', 0, 0).setScale(1);
 	worldLayer.setCollisionByExclusion([ -1 ]);
+
 	this.anims.create({
 		key: 'left',
 		frames: this.anims.generateFrameNumbers('dude', { start: 8, end: 13 }),
@@ -133,21 +284,45 @@ function create() {
 		repeat: 0
 	});
 	this.anims.create({
-		key: 'walk',
-		frames: this.anims.generateFrameNumbers('slime', { start: 3, end: 8 }),
+		key: 'slime-walk',
+		frames: this.anims.generateFrameNumbers('slime', { start: 0, end: 7 }),
 		frameRate: 8,
 		repeat: -1
 	});
 	this.anims.create({
-		key: 'attack',
+		key: 'slime-attack',
 		frames: this.anims.generateFrameNumbers('slime', { start: 8, end: 12 }),
 		frameRate: 8,
 		repeat: 0
 	});
 	this.anims.create({
-		key: 'die',
-		frames: this.anims.generateFrameNumbers('slime', { start: 16, end: 21 }),
+		key: 'slime-die',
+		frames: this.anims.generateFrameNumbers('slime', { start: 16, end: 20 }),
 		frameRate: 8,
+		repeat: 0
+	});
+	this.anims.create({
+		key: 'minotaur-walk',
+		frames: this.anims.generateFrameNumbers('minotaur', { start: 10, end: 17 }),
+		frameRate: 8,
+		repeat: 0
+	});
+	this.anims.create({
+		key: 'minotaur-idol',
+		frames: this.anims.generateFrameNumbers('minotaur', { start: 50, end: 60 }),
+		frameRate: 5,
+		repeat: 0
+	});
+	this.anims.create({
+		key: 'minotaur-attack',
+		frames: this.anims.generateFrameNumbers('minotaur', { start: 30, end: 34 }),
+		frameRate: 5,
+		repeat: 0
+	});
+	this.anims.create({
+		key: 'minotaur-die',
+		frames: this.anims.generateFrameNumbers('minotaur', { start: 90, end: 95 }),
+		frameRate: 5,
 		repeat: 0
 	});
 	this.anims.create({
@@ -192,27 +367,37 @@ function create() {
 	});
 	hearts = this.add.sprite(20, 20, 'hearts').setScrollFactor(0).setOrigin(0, 0).setDisplaySize(170, 34);
 
-	player = this.physics.add.sprite(6000, 150, 'dude').setDisplaySize(124, 92);
+	player = this.physics.add.sprite(100, 600, 'dude').setDisplaySize(124, 92);
+
 	player.justHurt = false;
 	player.justFired = false;
-
+	player.bow = false;
+	player.activate = function() {
+		player.bow = true;
+		bow.destroy();
+		keyHold.visible = true;
+		keyY.visible = true;
+	};
+	arrow = new Arrow(this, 0, 793);
 	enemies = this.add.group();
-
-	slimes = new Slime(this, 2000, 300);
-	slime2 = new Slime(this, 3000, 300);
+	slimes = new Slime(this, 1700, 300);
+	slime2 = new Slime(this, 2200, 300);
+	minotaur1 = new Minotaur(this, 3700, 100);
+	minotaur2 = new Minotaur(this, 5600, 100);
 	enemies.add(slimes);
 	enemies.add(slime2);
-	platform1 = this.physics.add.image(4380, 500, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
+	enemies.add(minotaur1);
+	enemies.add(minotaur2);
+	platform1 = this.physics.add.image(4380, 490, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
 	platform1.body.setAllowGravity(false);
-	platform2 = this.physics.add.image(4380, 550, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
+	platform2 = this.physics.add.image(4380, 540, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
 	platform2.body.setAllowGravity(false);
 	platform3 = this.physics.add.image(4880, 500, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
 	platform3.body.setAllowGravity(false);
 	platform4 = this.physics.add.image(4880, 550, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
 	platform4.body.setAllowGravity(false);
-	platform5 = this.physics.add.image(5800, 400, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
+	platform5 = this.physics.add.image(5500, 350, 'platform').setImmovable(true).setOrigin(0.5, 0.5);
 	platform5.body.setAllowGravity(false);
-
 	this.tweens.timeline({
 		targets: [ platform1.body.velocity, platform3.body.velocity ],
 		loop: -1,
@@ -273,90 +458,137 @@ function create() {
 			}
 		]
 	});
-	this.tweens.timeline({
-		targets: [ platform5.body.velocity ],
-		loop: -1,
-		tweens: [
-			{
-				x: 0,
-				y: 0,
-				duration: 700,
-				ease: 'Stepped'
-			},
-			{
-				x: -150,
-				y: 0,
-				duration: 3000,
-				ease: 'Stepped'
-			},
-			{
-				x: 0,
-				y: 0,
-				duration: 700,
-				ease: 'Stepped'
-			},
-			{
-				x: 150,
-				y: 0,
-				duration: 3000,
-				ease: 'Stepped'
-			}
-		]
+	puzzlePlatform = this.tweens.add({
+		targets: platform5,
+		yoyo: true,
+		repeat: -1,
+		x: 5500,
+		y: 580,
+		duration: 1500,
+		paused: true
 	});
-	doors = this.physics.add.sprite(6280, 315, 'door').setImmovable(true).setScale(2.3);
-	doors.body.setAllowGravity(false);
-	doors.open = function() {};
+	door0 = this.physics.add.sprite(4080, 360, 'door').setImmovable(true).setScale(2.3);
+	door0.body.setAllowGravity(false);
+	door0.open = function() {
+		this.setFrame(1);
+		this.body.setEnable(false);
+	};
+	door0.close = function() {
+		this.setFrame(0);
+		this.body.setEnable(true);
+	};
+	door1 = this.physics.add.sprite(6280, 315, 'door').setImmovable(true).setScale(2.3);
+	door1.body.setAllowGravity(false);
+	door1.open = function() {
+		this.setFrame(1);
+		this.body.setEnable(false);
+	};
+	door1.close = function() {
+		this.setFrame(0);
+		this.body.setEnable(true);
+	};
 
+	door2 = this.physics.add.sprite(6550, 680, 'door').setImmovable(true).setScale(2.3);
+	door2.body.setAllowGravity(false);
+	door2.open = function() {
+		this.setFrame(1);
+		this.body.setEnable(false);
+	};
+	door2.close = function() {
+		this.setFrame(0);
+		this.body.setEnable(true);
+	};
+	switch0 = this.physics.add.sprite(3520, 560, 'switch').setImmovable(true).setScale(4);
+	switch0.body.setAllowGravity(false);
+	switch0.active = false;
+	switch0.hit = function() {
+		if (this.active == false) {
+			this.setFrame(1);
+			this.active = true;
+			door0.open();
+		} else {
+			this.setFrame(0);
+			this.active = false;
+			door0.close();
+		}
+	};
+	switch1 = this.physics.add.sprite(6110, 610, 'switch').setImmovable(true).setScale(4);
+	switch1.body.setAllowGravity(false);
+	switch1.active = false;
+	switch1.hit = function() {
+		if (this.active == false) {
+			this.setFrame(1);
+			this.active = true;
+		} else {
+			this.setFrame(0);
+			this.active = false;
+		}
+		if (door1.body.enable == true) {
+			door1.open();
+		} else {
+			door1.close();
+		}
+		if (door2.body.enable == true) {
+			door2.open();
+		} else {
+			door2.close();
+		}
+		if (puzzlePlatform.isPlaying()) {
+			puzzlePlatform.pause();
+		} else {
+			puzzlePlatform.resume();
+		}
+	};
+
+	switch2 = this.physics.add.sprite(6193, 710, 'switch').setImmovable(true).setScale(4);
+	switch2.body.setAllowGravity(false);
+	switch2.active = false;
+	switch2.hit = function() {
+		if (this.active == false) {
+			this.setFrame(1);
+			this.active = true;
+		} else {
+			this.setFrame(0);
+			this.active = false;
+		}
+		if (door1.body.enable == true) {
+			door1.open();
+		} else {
+			door1.close();
+		}
+	};
 	player.setCollideWorldBounds(true);
 	player.setSize(20, 30).setOffset(15, 5);
+
+	//
+	this.physics.add.collider(arrow, enemies.getChildren(), hit, null, this);
+	player.setCollideWorldBounds(true);
+	player.setSize(20, 30).setOffset(15, 5);
+	this.physics.add.collider(arrow, [ switch0, switch1, switch2, door0, door1, door2 ], hit, null, this);
+	this.physics.add.collider(
+		arrow,
+		[ worldLayer, platform1, platform2, platform3, platform4, platform5, door0, door1, door2 ],
+		hit,
+		null,
+		this
+	);
+	this.physics.add.collider(enemies.getChildren(), player, attack, null, this);
 	this.physics.add.collider(worldLayer, [ player, enemies ]);
-	this.physics.add.collider([ platform1, platform2, platform3, platform4, platform5, doors ], player);
+	this.physics.add.overlap(player, bow, player.activate);
+	this.physics.add.collider([ platform1, platform2, platform3, platform4, platform5, door0, door1, door2 ], player);
 
-	function hit() {
-		/* arrow hit action */
+	function hit(arrow, target) {
+		setTimeout(() => {
+			arrow.setPosition(0, 793);
+		}, 100);
+		target.hit();
 	}
-	var Arrow = new Phaser.Class({
-		Extends: Phaser.GameObjects.Image,
-
-		initialize: function Arrow(scene) {
-			Phaser.GameObjects.Image.call(this, scene, 0, 0, 'arrow');
-
-			this.speed = Phaser.Math.GetSpeed(900, 1);
-		},
-
-		fire: function(x, y, flipX) {
-			if (flipX == true) {
-				this.setPosition(x - 30, y + 5);
-				this.flipX = true;
-			} else {
-				this.setPosition(x + 30, y + 5);
-			}
-			this.flip = flipX;
-			this.setActive(true);
-			this.setVisible(true);
-		},
-
-		update: function(time, delta) {
-			if (this.flip == true) {
-				this.x -= this.speed * delta;
-			} else {
-				this.x += this.speed * delta;
-			}
-
-			if (this.x < -50) {
-				this.setActive(false);
-				this.setVisible(false);
-			}
+	function attack(enemy, player) {
+		enemy.attack();
+		if (player.anims.currentAnim.key == 'slash') {
+			enemy.hurt();
 		}
-	});
-	arrows = this.add.group({
-		classType: Arrow,
-		maxSize: 10,
-		runChildUpdate: true
-	});
-
-	worldLayer.setTileIndexCallback(13, hit, this);
-
+	}
 	player.health = 10;
 	player.heal = function() {
 		if (player.health < 10) {
@@ -366,16 +598,22 @@ function create() {
 
 	player.hurt = function() {
 		if (player.health > 0 && player.justHurt == false && !cursors.down.isDown) {
+			player.setTintFill(0xffffff);
+			setTimeout(() => {
+				player.clearTint();
+			}, 100);
 			player.justHurt = true;
 			setTimeout(function() {
 				player.justHurt = false;
 			}, 500);
 			player.setVelocityY(-300);
 			return --player.health;
-		} else {
-			console.log('endgame');
+		} else if (player.health == 0) {
+			location.reload();
 		}
 	};
+	worldLayer.setTileIndexCallback(13, player.hurt, this);
+	worldLayer.setTileIndexCallback(12, player.hurt, this);
 	var particles = this.add.particles('spark');
 	egg = this.physics.add.sprite(7250, 120, 'egg').setOrigin(0, 0).setDisplaySize(60, 77);
 	emitter = particles.createEmitter({ scale: { start: 0.1, end: 0 } });
@@ -390,6 +628,7 @@ function create() {
 	this.background0 = this.add.tileSprite(0, 0, 8000, config.height, 'background-0').setOrigin(0, 0);
 	this.cameras.main.setBounds(0, 0, 8000, 793);
 	this.cameras.main.startFollow(player, true, 0.01, 0.01);
+	door1.open();
 }
 action = 'idol';
 var previousPosition;
@@ -477,11 +716,8 @@ function playerControls() {
 	//shoot
 	if (player.anims.currentFrame.index == 8 && player.anims.currentAnim.key == 'shoot' && player.justFired == false) {
 		player.justFired = true;
-		var arrow = arrows.get();
 		arrow.fire(player.x, player.y, player.flipX);
-		doors.setFrame(1);
-		doors.body.setEnable(false);
-		console.dir(doors);
+
 		setTimeout(function() {
 			player.justFired = false;
 		}, 500);
@@ -496,7 +732,8 @@ function playerControls() {
 	if (
 		Phaser.Input.Keyboard.JustDown(z) &&
 		(player.body.onFloor() || player.body.touching.down) &&
-		(action == 'shoot' || action == 'idol')
+		(action == 'shoot' || action == 'idol') &&
+		player.bow == true
 	) {
 		action = 'shoot';
 		player.setVelocityX(0);
